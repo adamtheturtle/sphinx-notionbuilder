34f5c44c6dac8ba5a06786616619b1fe12f1de76
Adam Dangoor <adamdangoor@gmail.com>
2025-10-02 10:46:04 +0100

    Move pyright ignore

--- src/sphinx_notion/__init__.py
+++ src/sphinx_notion/__init__.py
@@ -21,8 +21,8 @@ from sphinx.builders.text import TextBuilder
 from sphinx.util import docutils as sphinx_docutils
 from sphinx.util import logging as sphinx_logging
 from sphinx.util.typing import ExtensionMetadata
-from sphinx_immaterial.task_lists import (
-    checkbox_label,  # pyright: ignore[reportMissingTypeStubs]
+from sphinx_immaterial.task_lists import (  # pyright: ignore[reportMissingTypeStubs]
+    checkbox_label,
 )
 from sphinx_simplepdf.directives.pdfinclude import (  # pyright: ignore[reportMissingTypeStubs]
     PdfIncludeDirective,
