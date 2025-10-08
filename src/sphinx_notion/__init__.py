1ec783881a0e5fc331eb81d78f43770a9ab0ff1f
Adam Dangoor <adamdangoor@gmail.com>
2025-10-08 11:10:22 +0100

    Ignore pyright issue for sphinx-iframes

--- src/sphinx_notion/__init__.py
+++ src/sphinx_notion/__init__.py
@@ -22,7 +22,9 @@ from sphinx.builders.text import TextBuilder
 from sphinx.util import docutils as sphinx_docutils
 from sphinx.util import logging as sphinx_logging
 from sphinx.util.typing import ExtensionMetadata
-from sphinx_iframes import iframe_node
+from sphinx_iframes import (  # pyright: ignore[reportMissingTypeStubs]
+    iframe_node,
+)
 from sphinx_immaterial.task_lists import checkbox_label
 from sphinx_simplepdf.directives.pdfinclude import (  # pyright: ignore[reportMissingTypeStubs]
     PdfIncludeDirective,
