397e74359ae2604648ceffc3bf44d077f73aef1e
Adam Dangoor <adamdangoor@gmail.com>
2026-02-08 16:49:44 +0000

    Rename sample-warnings to sample_warnings

--- publish-sample.sh
+++ publish-sample.sh
@@ -18,11 +18,11 @@ uv run --all-extras notion-upload \
     --title "Test page title during testing" \
     --icon "🐍"
 
-rm -rf "${SCRIPT_DIR}/build-sample-warnings"
-uv run --extra=sample sphinx-build -W -b notion "${SCRIPT_DIR}/sample-warnings" "${SCRIPT_DIR}/build-sample-warnings"
+rm -rf "${SCRIPT_DIR}/build-sample_warnings"
+uv run --extra=sample sphinx-build -W -b notion "${SCRIPT_DIR}/sample_warnings" "${SCRIPT_DIR}/build-sample_warnings"
 
 uv run --all-extras notion-upload \
     --parent-database-id "$NOTION_SAMPLE_DATABASE_ID" \
-    --file "${SCRIPT_DIR}/build-sample-warnings/index.json" \
+    --file "${SCRIPT_DIR}/build-sample_warnings/index.json" \
     --title "Test suppressed warnings page title during testing" \
     --icon "🐍"
